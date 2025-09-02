069ad78da5b82f7383135c6e68edbea92b158b1f
Mohankumar Saraswathy <msaraswathy@siddhaai.com>
2025-09-03 01:39:22 +0530

    Add meta tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <p>Div Element</p>
-    </div>
+   <div>
+  <ul>
+    <li><a href="/home">Homepage</a></li>
+    <li><a href="/about">About Us</a></li>
+    <li><a href="/blog">Blog</a></li>
+  </ul>
+</div>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <head>
+            <meta name="description" content="little-lemon-restaurant"/>
+            <meta name="og:title" content="Little Lemon"/>
+            <meta name="og:image" content="Little Lemon Logo"/>
+        </head>
+    )
+}
+
+export default Header;
